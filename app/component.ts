3d6e47d6f17fa85b8743722a805b78c77def86d8
mimuuse <mohamed.musse@golis.so>
2017-10-02 08:22:02 +0300

    initial

---
+++ app/component.ts
@@ -0,0 +1,34 @@
+﻿import { ApplicationRef, Component } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
+@Component({
+    selector: "app",
+    templateUrl: "app/template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+    getProduct(key: number): Product {
+        return this.model.getProduct(key);
+    }
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+    newProduct: Product = new Product();
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
+    }
+    addProduct(p: Product) {
+        //console.log("New Product: " + this.jsonProduct);
+        this.model.addProduct(this.newProduct);
+    }
+    deleteProduct(index: number){
+        this.model.deleteProduct(index);
+    }
+    updateProduct(p: Product) {
+        //console.log("New Product: " + this.jsonProduct);
+        this.model.saveProduct(this.newProduct);
+    }
+    selectProduct(index: number){
+        this.newProduct = this.model.getProduct(index);
+    }
+}
